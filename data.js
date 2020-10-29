e8f5341e9908c2d0fb14b3a48baeead52968d704
librits <librits19@gmail.com>
2020-10-29 16:21:21 +0800

    init

---
+++ data.js
@@ -0,0 +1,286 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrance",
+      "name": "Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 2.4641047868015153,
+        "pitch": 0.4192519263652379,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.161825051727572,
+          "pitch": 0.04131538533908774,
+          "rotation": 0,
+          "target": "3-thinktank"
+        },
+        {
+          "yaw": 0.8630902135669114,
+          "pitch": 0.17788665579655039,
+          "rotation": 11.780972450961727,
+          "target": "1-conference-room"
+        },
+        {
+          "yaw": -2.5139275521988225,
+          "pitch": 0.08676706659005795,
+          "rotation": 0,
+          "target": "2-book-sorting-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-conference-room",
+      "name": "Conference room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 1.9048012268726886,
+        "pitch": -0.07865103723519162,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-book-sorting-room",
+      "name": "Book Sorting Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-thinktank",
+      "name": "ThinkTank",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -2.973897237096807,
+        "pitch": -0.05161037900625942,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3173948755507414,
+          "pitch": -0.035051668187001894,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": 1.317055883440453,
+          "pitch": 0.034991590649637416,
+          "rotation": 0.7853981633974483,
+          "target": "6-kitchen"
+        },
+        {
+          "yaw": 0.6135286515322491,
+          "pitch": -0.10960652033327101,
+          "rotation": 0,
+          "target": "4-studio"
+        },
+        {
+          "yaw": -0.8819009903617445,
+          "pitch": 0.04893240800013032,
+          "rotation": 0,
+          "target": "5-vr-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-studio",
+      "name": "Studio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 2.7669215204026205,
+        "pitch": 0.006682225233635819,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3140039380026476,
+          "pitch": -0.1816222652993158,
+          "rotation": 13.351768777756625,
+          "target": "3-thinktank"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-vr-room",
+      "name": "VR Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.8266193199887049,
+        "pitch": 0.0804161900166207,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 1.2338689717350046,
+        "pitch": 0.0741630841990677,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.58332539408274,
+          "pitch": 0.15860845148005964,
+          "rotation": 0,
+          "target": "3-thinktank"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Library Office Virtual Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
